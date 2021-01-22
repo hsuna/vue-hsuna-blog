98e73e2d732cc30416e43a68f7aa507afe00a137
hsuna <me@hsuna.com>
2021-01-22 15:20:50 +0800

    tmp

--- vue.config.js
+++ vue.config.js
@@ -14,13 +14,16 @@ module.exports = {
         { name: 'essay', title: '手札 | HSUAN' },
         { name: 'index', title: '首页 | HSUAN' },
     ].reduce((obj, item) => Object.assign(obj, { 
-        [item.name]: {
-            entry: resolve(`src/pages/${item.name}/main.js`), // page 的入口,每个“page”应该有一个对应的 JavaScript 入口文件
-            template: 'public/index.html',                    // 模板来源
-            filename: `${item.name}.html`,                    // 在 dist/index.html 的输出
-            title: item.title || 'BLOG | HSUAN',              // 当使用 title 选项时,在 template 中使用：<title><%= htmlWebpackPlugin.options.title %></title>
-            chunks: ['common', item.name]          // 在这个页面中包含的块，默认情况下会包含,提取出来的 chunks
-        }
+        [item.name]: 
+            process.env.NODE_ENV === 'production'
+            ? {
+                entry: resolve(`src/pages/${item.name}/main.js`), // page 的入口,每个“page”应该有一个对应的 JavaScript 入口文件
+                template: 'public/index.html',                    // 模板来源
+                filename: `${item.name}.html`,                    // 在 dist/index.html 的输出
+                title: item.title || 'BLOG | HSUAN',              // 当使用 title 选项时,在 template 中使用：<title><%= htmlWebpackPlugin.options.title %></title>
+                chunks: ['common', item.name]          // 在这个页面中包含的块，默认情况下会包含,提取出来的 chunks
+            }
+            : resolve(`src/pages/${item.name}/main.js`),
     }), {}),
     publicPath: './',
     assetsDir: 'static',//放置生成的静态资源(s、css、img、fonts)的(相对于 outputDir 的)目录(默认'')
